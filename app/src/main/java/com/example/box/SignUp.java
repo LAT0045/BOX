ec718d7d7bea88cc5a384b9070da46a66d4204d0
quynhnnt <thuyquynh311002@gmail.com>
2023-04-20 17:42:24 +0700

    Add Sign Up UI

---
+++ app/src/main/java/com/example/box/SignUp.java
@@ -0,0 +1,39 @@
+package com.example.box;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class SignUp extends AppCompatActivity {
+    private FrameLayout container;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();
+
+        // Initialize UI
+        initializeUI();
+
+        // Initialize fragments
+        SignUpInfoFragment signUpInfoFragment = new SignUpInfoFragment();
+
+        // Initialize Fragment Manager
+        fragmentManager = getSupportFragmentManager();
+
+        // Make SignUpInfoFragment first fragment
+        // Add to back stack with "signUpInfoFragment" tag
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, signUpInfoFragment, "signUpInfoFragment")
+                .addToBackStack("signUpInfoFragment")
+                .commit();
+    }
+
+    private void initializeUI() {
+        container = (FrameLayout) findViewById(R.id.container);
+    }
+}
